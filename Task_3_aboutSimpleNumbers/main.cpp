fbfe258be28269e73a15142425d7548922aff8e1
Maksim Merzhew <79693216+licenzed500-svg@users.noreply.github.com>
2024-05-12 23:50:56 +0300

    Add files via upload

---
+++ Task_3_aboutSimpleNumbers/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    int sqrtNum = std::sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+std::vector<int> getPrimesInRange(int min, int max) {
+    std::vector<int> primes;
+    for (int i = min; i <= max; i++) {
+        if (isPrime(i)) primes.push_back(i);
+    }
+    return primes;
+}
+
+int main() {
+    int min = 11;
+    int max = 20;
+    std::vector<int> primes = getPrimesInRange(min, max);
+    std::cout << "Простые числа в диапазоне от " << min << " до " << max << ":\n";
+    for (int prime : primes) {
+        std::cout << prime << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
